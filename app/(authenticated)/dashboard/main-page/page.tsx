c1bf835e05df274b4297c9c886dd19994964e655
gregllanes <mgllanes20@gmail.com>
2024-06-02 16:30:51 +0800

    feat: updated main and secondary page

--- app/(authenticated)/dashboard/main-page/page.tsx
+++ app/(authenticated)/dashboard/main-page/page.tsx
@@ -1,5 +1,27 @@
 import React from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function page() {
-  return <div>page</div>;
+  return (
+    <Card className="bg-[#28527A]">
+      <CardContent className="grid grid-cols-2">
+        <div className="flex flex-row justify-center items-center col-span-1">
+          Table 1
+        </div>
+        <div className="flex flex-row justify-center items-center col-span-1">
+          Table 2
+        </div>
+      
+      </CardContent>
+    </Card>
+  );
 }
