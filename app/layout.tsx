5072039096afbb0e9a8bb05af8f934208e14c486
gregllanes <mgllanes20@gmail.com>
2024-06-02 14:07:00 +0800

    feat: staging login and signup

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="grid grid-cols-12 h-screen">
+          <div className="col-span-6 bg-[#8AC4D0] "></div>
+          <div className="col-span-6 bg-[#28527A] "></div>
+          <div className="absolute inset-0 flex justify-center items-center">
+            <div className="bg-white p-8 shadow-lg rounded-lg">
+              {/* Card content here */}
+              {children}
+              This is a centered card.
+            </div>
+          </div>
+        </div>
+      </body>
     </html>
   );
 }
