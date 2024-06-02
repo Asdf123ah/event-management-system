d20864d924d9dfb3872029fbea4a93d322a8d333
gregllanes <mgllanes20@gmail.com>
2024-06-02 16:36:05 +0800

    fix: navbar using the secondary page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,22 +1,32 @@
+"use client";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const Navbar = () => {
+  const pathName = usePathname();
   return (
     <nav className="bg-[#28527A] text-white px-4 py-2 relative z-10 h-16">
       <div className="flex justify-between items-center h-full">
         <div className="relative">
           <div className="ml-8 bg-white">
-            <Image
-              src={"/images/header.png"}
-              alt="Logo"
-              width={200}
-              height={125}
-              /*  className="w-[200px] h-auto" // Adjust as needed */
-            />
+            {pathName !== "/dashboard/secondary-page" ? (
+              <Image
+                src={"/images/header.png"}
+                alt="Logo"
+                width={200}
+                height={125}
+              />
+            ) : (
+              <Image
+                src={"/images/brgy.png"}
+                alt="Logo"
+                width={200}
+                height={125}
+              />
+            )}
           </div>
         </div>
-       
       </div>
     </nav>
   );
