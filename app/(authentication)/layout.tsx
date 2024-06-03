8992e3b8dc8be5818eae462eed7e356a4c426bbb
xcutiepatootie <mackycalabia@gmail.com>
2024-06-03 21:12:25 +0800

    feat: added toast notif

--- app/(authentication)/layout.tsx
+++ app/(authentication)/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../globals.css";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,6 +26,7 @@ export default function RootLayout({
             {children}
           </div>
         </div>
+        <Toaster />
       </body>
     </html>
   );
