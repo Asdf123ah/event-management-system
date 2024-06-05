346444acc2e801e32cd59b46f8324c4057c58c8c
beejayreyes <beejay.reyes0112@gmail.com>
2024-06-06 03:45:53 +0800

    feat: dashboard skeleton

--- app/(authenticated)/dashboard/loading.tsx
+++ app/(authenticated)/dashboard/loading.tsx
@@ -1,7 +1,8 @@
+import Dashboard_Skeleton from '@/components/Skeletons/dashboard/Dashboard_Skeleton'
 import React from 'react'
 
 export default function loading() {
   return (
-    <div>loading</div>
+    <Dashboard_Skeleton />
   )
 }
