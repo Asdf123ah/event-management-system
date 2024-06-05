232eef033e919ce28231bd80bdd0706463832933
beejayreyes <beejay.reyes0112@gmail.com>
2024-06-05 21:43:14 +0800

    feat: secondary page skeleton

---
+++ app/(authenticated)/dashboard/secondary-page/loading.tsx
@@ -0,0 +1,8 @@
+import Secondary_Skeleton from '@/components/Skeletons/dashboard/secondary-page/Secondary_Skeleton'
+import React from 'react'
+
+export default function loading() {
+  return (
+    <Secondary_Skeleton/>
+  )
+}
